44046bb5d330f28b5f1909aafa0368c87b2030bb
Oneil <oneilswilliams@gmail.com>
2020-10-20 12:33:47 -0400

    update

--- backend-master/app.js
+++ backend-master/app.js
@@ -10,26 +10,24 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-
 const accountsRoute = require('./routes/accounts')
 const profilesRoute = require('./routes/profiles')
 const bankingsRoute = require('./routes/bankings')
 const cardsRoute = require('./routes/cards')
 
+mongoose.connect(process.env.DB_CONNECTION, 
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true },
+  ()=>{console.log("Connected to the DB");}
+)
 
 app.use('/accounts', accountsRoute)
 app.use('/profiles', profilesRoute)
 app.use('/bankings', bankingsRoute)
 app.use('/cards', cardsRoute)
 
-
 app.get('/', (req, res) => {
-  res.send('Hola!')
+  res.send('Welcome to Iuncta!')
 })
 
-mongoose.connect(process.env.DB_CONNECTION, 
-  { useNewUrlParser: true },
-  ()=>{console.log("Connected to DB");}
-)
-
- app.listen(8000)
+ app.listen(80)
