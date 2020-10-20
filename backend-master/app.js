b037460d79a65237c6eeac6895b0608e92b554d5
Oneil <oneilswilliams@gmail.com>
2020-10-20 14:23:15 -0400

    little edits

--- backend-master/app.js
+++ backend-master/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path');
+const server = require('http').createServer();
 require('dotenv/config')
 
 app.use(cors())
@@ -15,19 +16,23 @@ const profilesRoute = require('./routes/profiles')
 const bankingsRoute = require('./routes/bankings')
 const cardsRoute = require('./routes/cards')
 
-mongoose.connect(process.env.DB_CONNECTION, 
-  { useNewUrlParser: true },
-  { useUnifiedTopology: true },
-  ()=>{console.log("Connected to the DB");}
-)
-
 app.use('/accounts', accountsRoute)
 app.use('/profiles', profilesRoute)
 app.use('/bankings', bankingsRoute)
 app.use('/cards', cardsRoute)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Iuncta!')
+  res.send('Welcome To Iuncta API!')
 })
 
- app.listen(80)
+mongoose.connect(process.env.DB_CONNECTION, 
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true },
+  ()=>{console.log("Connected to DB");}
+)
+
+const port = process.env.PORT || 8000;
+ app.listen(port)
+//  server.listen(port, () => console.log(`Listening on ${port}`));
+
+module.exports = app
